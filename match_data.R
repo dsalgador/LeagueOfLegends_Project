1a06b7df8bc696481d272752c9b3038a0b5e530e
Daniel Salgado Rojo <daniel.salgado.rojo@gmail.com>
2017-10-27 17:57:02 +0200

    Add match_data.R: building the train data structure for when doing the logistic regression. Done just for 1 match.

---
+++ match_data.R
@@ -0,0 +1,61 @@
+setwd("C:/Users/Daniel/Dropbox/GitHub/LeagueOfLegends_Model")
+
+# set bolean to 1 and coment the line
+#bolean <- 1
+if(bolean == 1){
+  #df <- read.csv("2016_CompleteMatchData.csv", header = T,na.strings=c(""))
+  df <- read.csv("2017_WorldsMatchData.csv", header = T,na.strings=c(""))
+  bolean = 0;
+}
+
+library(data.table)
+names(df)
+df <- as.data.table(df)
+df <- df[league=="WC"]
+
+
+# library(Amelia)
+# missmap(df, main = "Missing values vs observed")
+
+fields <- c("result", "gameid", "playerid", "side", "position", "player", "team", "champion", "gamelength")
+dfs <- subset(df, select = fields)
+
+
+game_ids <- unique(dfs$"gameid")
+game <- dfs[gameid == game_ids[1]]
+
+game_team1 <- game[side == 'Blue'][1:5]; 
+game_team2 <- game[side == 'Red'][1:5]
+
+
+#c(ADC, SUP, MID, JNG, TOP).
+team1_champs <- as.character(game_team1$champion)
+team1_players <- as.character(game_team1$player)
+team2_champs <- as.character(game_team2$champion)
+team2_players <- as.character(game_team2$player)
+
+
+##champ-matchup-winrate  for each of the 5 positions. From champion.gg data
+team1_champwr = winrate_match(team1_champs, team2_champs)
+team2_champwr = 1-team1_champwr
+
+##champ-player-winrate for each of the 5 players of the team.
+#Using data obtained scraping lol.gamepedia.com/player_name/Champion_Statistics
+
+#TO DO
+
+####
+
+match_team1 <- data.table(result = game_team1$result[1], team = game_team1$team[1], side = game_team1$side[1])
+match_team2 <- data.table(result = game_team2$result[1], team = game_team2$team[1], side = game_team2$side[1])
+
+poswr_names <- c('topwr_champ', 'jngwr_champ', 'midwr_champ', 'adcwr_champ', 'supwr_champ')
+poswr_list1 <- list()
+poswr_list2 <- list()
+for(i in 1:5){poswr_list1[[i]]= team1_champwr[i];poswr_list2[[i]]= team2_champwr[i]; }
+match_team1[,c(poswr_names):=poswr_list1]
+match_team2[,c(poswr_names):=poswr_list2]
+
+#match_team1[,players:= as.character(game_team1$player)]
+#match_team1[,champs:= as.character(game_team1$champion)]
+
